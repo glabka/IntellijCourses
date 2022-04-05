7df0d4a3da02b077f7d7c7742e82832061160735
glabka <glabka333@gmail.com>
2022-04-05 22:48:19 +0200

    Functional solution based on coursera.

--- Kotlin for Java Developers. Week 2/Mastermind/Task/src/mastermind/evaluateGuess.kt
+++ Kotlin for Java Developers. Week 2/Mastermind/Task/src/mastermind/evaluateGuess.kt
@@ -3,25 +3,12 @@ package mastermind
 data class Evaluation(val rightPosition: Int, val wrongPosition: Int)
 
 fun evaluateGuess(secret: String, guess: String): Evaluation {
-    val secretChars = secret.map { it }
-    val guessChars = guess.map { it }
-    val numOfSecretChars: Map<Char, Int> =
-        secretChars.associateWith { character -> secretChars.filter { it == character }.size }
-    val numOfGuessChars: Map<Char, Int> =
-        guessChars.associateWith { character -> guessChars.filter { it == character }.size }
-    val sameChars = secretChars.zip(guessChars).filter { (sCh, gCh) -> sCh == gCh }
-    val numOfCorrectChars: Map<Char, Int> =
-        secretChars.associateWith { ch -> sameChars.filter { (character, _) -> character == ch }.size }
-    val numOfWrongPositions: Map<Char, Int> = numOfSecretChars.toList().associateBy({ it.first },
-        {
-            Integer.min(
-                (numOfGuessChars[it.first] ?: 0) - (numOfCorrectChars[it.first] ?: 0),
-                (it.second - (numOfCorrectChars[it.first] ?: 0))
-            )
-        })
 
-    val rightPosition = numOfCorrectChars.values.sum()
-    val wrongPosition = numOfWrongPositions.values.sum()
+    val rightPositions = secret.zip(guess).count { it.first == it.second }
 
-    return Evaluation(rightPosition, wrongPosition)
+    val commonLetters = "ABCDEF".sumBy { ch ->
+
+        Math.min(secret.count { it == ch }, guess.count { it == ch })
+    }
+    return Evaluation(rightPositions, commonLetters - rightPositions)
 }
