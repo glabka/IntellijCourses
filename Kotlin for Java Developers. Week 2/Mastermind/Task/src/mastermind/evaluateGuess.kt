00ff60bfc55957e263273557ec99254eb48b9176
glabka <glabka333@gmail.com>
2022-04-05 22:35:48 +0200

    Functional approach to mastermind game evaluation.

--- Kotlin for Java Developers. Week 2/Mastermind/Task/src/mastermind/evaluateGuess.kt
+++ Kotlin for Java Developers. Week 2/Mastermind/Task/src/mastermind/evaluateGuess.kt
@@ -3,51 +3,25 @@ package mastermind
 data class Evaluation(val rightPosition: Int, val wrongPosition: Int)
 
 fun evaluateGuess(secret: String, guess: String): Evaluation {
-    val sOccurrencesAndCorrectness = getOccurrencesAndCorrectness(secret, guess)
-    val guessLetterOccurrences = getLetterOccurrences(guess)
-    var rightPosition = 0
-    var wrongPosition = 0
-    for ((ch, pair) in sOccurrencesAndCorrectness) {
-        rightPosition += pair.second
-        val guessedOc = numOfGuessedOc(pair.first, guessLetterOccurrences[ch] ?: 0)
-        wrongPosition += guessedOc - pair.second
-    }
+    val secretChars = secret.map { it }
+    val guessChars = guess.map { it }
+    val numOfSecretChars: Map<Char, Int> =
+        secretChars.associateWith { character -> secretChars.filter { it == character }.size }
+    val numOfGuessChars: Map<Char, Int> =
+        guessChars.associateWith { character -> guessChars.filter { it == character }.size }
+    val sameChars = secretChars.zip(guessChars).filter { (sCh, gCh) -> sCh == gCh }
+    val numOfCorrectChars: Map<Char, Int> =
+        secretChars.associateWith { ch -> sameChars.filter { (character, _) -> character == ch }.size }
+    val numOfWrongPositions: Map<Char, Int> = numOfSecretChars.toList().associateBy({ it.first },
+        {
+            Integer.min(
+                (numOfGuessChars[it.first] ?: 0) - (numOfCorrectChars[it.first] ?: 0),
+                (it.second - (numOfCorrectChars[it.first] ?: 0))
+            )
+        })
+
+    val rightPosition = numOfCorrectChars.values.sum()
+    val wrongPosition = numOfWrongPositions.values.sum()
+
     return Evaluation(rightPosition, wrongPosition)
-}
-
-fun numOfGuessedOc(secretOc: Int, guessOc: Int) : Int {
-    return if (secretOc > guessOc) guessOc else secretOc
-}
-fun Boolean.toInt() = if (this) 1 else 0
-
-fun getOccurrencesAndCorrectness(secret: String, guess: String) : Map<Char, Pair<Int, Int>> {
-    val occurrences: MutableMap<Char, Pair<Int, Int>> = mutableMapOf()
-    val charsSecret = secret.toCharArray();
-    val charsGuess= guess.toCharArray();
-    for ((index, ch) in charsSecret.withIndex()) {
-        val correct = ch == charsGuess[index]
-        // Pair? var has to be assigned to variable in order for compiler to smart-cast it
-        // into Pair - probably because of possible parallelization access to data
-        val pair = occurrences[ch]
-        if (pair == null) {
-            occurrences[ch] = Pair(1, correct.toInt())
-        } else {
-            occurrences[ch] = Pair(pair.first.plus(1), pair.second.plus(correct.toInt()))
-
-        }
-    }
-    return occurrences
-}
-
-fun getLetterOccurrences(str: String) : Map<Char,Int> {
-    val occurrences: MutableMap<Char, Int> = mutableMapOf()
-    val chars = str.toCharArray();
-    for (ch in chars) {
-        if (occurrences[ch] == null) {
-            occurrences[ch] = 1
-        } else {
-            occurrences[ch] = occurrences[ch]!! + 1
-        }
-    }
-    return occurrences
 }
