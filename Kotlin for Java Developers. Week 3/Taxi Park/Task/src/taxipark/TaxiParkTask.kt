44da6ec0fedf25977f8c847339c617b69b29b13d
glabka <glabka333@gmail.com>
2022-04-07 20:07:18 +0200

    week3's taxipark assignment - my solution without unnecessary this.

--- Kotlin for Java Developers. Week 3/Taxi Park/Task/src/taxipark/TaxiParkTask.kt
+++ Kotlin for Java Developers. Week 3/Taxi Park/Task/src/taxipark/TaxiParkTask.kt
@@ -6,27 +6,27 @@ import kotlin.math.floor
  * Task #1. Find all the drivers who performed no trips.
  */
 fun TaxiPark.findFakeDrivers(): Set<Driver> =
-        this.allDrivers.filter { driver -> this.trips.none { trip -> trip.driver == driver } }.toSet()
+        allDrivers.filter { driver -> trips.none { trip -> trip.driver == driver } }.toSet()
 
 /*
  * Task #2. Find all the clients who completed at least the given number of trips.
  */
 fun TaxiPark.findFaithfulPassengers(minTrips: Int): Set<Passenger> =
-    this.allPassengers.filter { passenger ->
-        this.trips.count {trip -> trip.passengers.contains(passenger) } >= minTrips}.toSet()
+    allPassengers.filter { passenger ->
+        trips.count {trip -> trip.passengers.contains(passenger) } >= minTrips}.toSet()
 
 /*
  * Task #3. Find all the passengers, who were taken by a given driver more than once.
  */
 fun TaxiPark.findFrequentPassengers(driver: Driver): Set<Passenger> =
-    this.allPassengers.filter { passenger -> this.trips.filter {
+    allPassengers.filter { passenger -> trips.filter {
             trip -> trip.passengers.contains(passenger) && trip.driver == driver }.size > 1 }.toSet()
 /*
  * Task #4. Find the passengers who had a discount for majority of their trips.
  */
 fun TaxiPark.findSmartPassengers(): Set<Passenger> =
-    this.allPassengers.filter { passenger ->
-        val (disTrips, nonDistTrips) = this.trips.filter { trip -> trip.passengers.contains(passenger) }
+    allPassengers.filter { passenger ->
+        val (disTrips, nonDistTrips) = trips.filter { trip -> trip.passengers.contains(passenger) }
         .partition { trip -> trip.discount ?: 0.0 > 0.0 }
         disTrips.size > nonDistTrips.size
     }.toSet()
@@ -35,16 +35,16 @@ fun TaxiPark.findSmartPassengers(): Set<Passenger> =
  * Return any period if many are the most frequent, return `null` if there're no trips.
  */
 fun TaxiPark.findTheMostFrequentTripDurationPeriod(): IntRange? {
-    if (this.trips == null || this.trips.isEmpty()) return null
-    return this.allRanges().filterNotNull().maxBy { range ->
-        this.trips.count {trip -> trip.duration in range} }
+    if (trips == null || trips.isEmpty()) return null
+    return allRanges().filterNotNull().maxBy { range ->
+        trips.count {trip -> trip.duration in range} }
 }
 
 fun TaxiPark.allRanges(): MutableList<IntRange?> {
     fun Int.toDecimal() : Int {
         return this + (10 - (this % 10))
     }
-    val maxRange = this.trips.maxBy { it.duration }?.duration?.toDecimal() ?: 0
+    val maxRange = trips.maxBy { it.duration }?.duration?.toDecimal() ?: 0
     val ranges: MutableList<IntRange?> = mutableListOf()
     for (rangeStart in 0..maxRange step 10) {
         ranges.add(rangeStart until rangeStart + 10)
@@ -56,11 +56,11 @@ fun TaxiPark.allRanges(): MutableList<IntRange?> {
  * Check whether 20% of the drivers contribute 80% of the income.
  */
 fun TaxiPark.checkParetoPrinciple(): Boolean {
-    if (this.trips == null || this.trips.isEmpty()) return false
-    val numOfTopDrivers = floor(this.allDrivers.size * 0.2)
-    val sortedDriversAndCosts = this.allDrivers.associateWith { driver -> this.trips.filter {trip -> trip.driver == driver}
+    if (trips == null || trips.isEmpty()) return false
+    val numOfTopDrivers = floor(allDrivers.size * 0.2)
+    val sortedDriversAndCosts = allDrivers.associateWith { driver -> trips.filter {trip -> trip.driver == driver}
         .sumByDouble { trip -> trip.cost }}.toList().sortedBy { (_, costs) -> costs }
-    val (restOfDrivers, topDrivers) = sortedDriversAndCosts.withIndex().partition { (index, _) -> index < this.allDrivers.size - numOfTopDrivers }
+    val (restOfDrivers, topDrivers) = sortedDriversAndCosts.withIndex().partition { (index, _) -> index < allDrivers.size - numOfTopDrivers }
     println(restOfDrivers)
     println(topDrivers)
     val allCosts = sortedDriversAndCosts.sumByDouble { (_, costs) -> costs }
