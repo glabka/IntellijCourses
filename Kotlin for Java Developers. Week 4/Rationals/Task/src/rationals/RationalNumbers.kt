e35b615854ae7afba7e33db80b8b47d550c119df
glabka <glabka333@gmail.com>
2022-04-15 21:41:31 +0200

    week4's rationals assignment - course solution.

--- Kotlin for Java Developers. Week 4/Rationals/Task/src/rationals/RationalNumbers.kt
+++ Kotlin for Java Developers. Week 4/Rationals/Task/src/rationals/RationalNumbers.kt
@@ -1,67 +1,56 @@
 package rationals
 
-import org.w3c.dom.ranges.Range
 import java.math.BigInteger
+import java.math.BigInteger.ONE
+import java.math.BigInteger.ZERO
 
-data class Rational(private var numerator: BigInteger, private var denominator: BigInteger) {
+data class Rational private constructor(val numerator: BigInteger, val denominator: BigInteger) : Comparable<Rational> {
 
-    init {
-        if (denominator == BigInteger.ZERO) throw IllegalArgumentException()
+    companion object {
+        fun createRational(numerator: BigInteger, denominator: BigInteger): Rational {
+            require(denominator != ZERO) { "Denominator can't be zero." }
 
-        // moving possible negative sign from denominator to numerator or dropping it completely if both are negative
-        val numNegative = numerator.compareTo(BigInteger.ZERO) < 0
-        val denomNegative = denominator.compareTo(BigInteger.ZERO) < 0
+            // normalization
+            val sign = denominator.signum().toBigInteger()
 
-        if (numNegative && denomNegative) {
-            numerator = numerator.abs()
-            denominator = denominator.abs()
-        } else if (denomNegative) {
-            numerator = numerator.negate()
-            denominator = denominator.negate()
+            // dividing by greatest common divisor
+            val gcd = numerator.gcd(denominator)
+            return Rational(numerator / gcd * sign, denominator / gcd * sign)
         }
 
-        // dividing by greatest common divisor
-        val gcd = numerator.gcd(denominator)
-        this.numerator = numerator / gcd
-        this.denominator = denominator / gcd
+        fun createRational(numerator: Int, denominator: Int): Rational {
+            return createRational(numerator.toBigInteger(), denominator.toBigInteger())
+        }
+
+        fun createRational(numerator: Long, denominator: Long): Rational {
+            return createRational(numerator.toBigInteger(), denominator.toBigInteger())
+        }
     }
 
-    constructor(numerator: Int, denominator: Int): this(numerator.toBigInteger(), denominator.toBigInteger())
-    constructor(numerator: Long, denominator: Long): this(numerator.toBigInteger(), denominator.toBigInteger())
+    operator fun times(rational: Rational): Rational
+    = createRational(numerator * rational.numerator,  denominator * rational.denominator)
 
-    operator fun times(rational: Rational): Rational {
-        val numerator = this.numerator * rational.numerator
-        val denominator = this.denominator * rational.denominator
-        return Rational(numerator, denominator)
-    }
+    operator fun unaryMinus(): Rational = createRational(-numerator, denominator)
    
-    operator fun unaryMinus(): Rational = Rational(-this.numerator, this.denominator)
-   
-    // short implementation 
-    operator fun minus(that: Rational): Rational = this + (-that)
+    operator fun minus(that: Rational): Rational = createRational(
+        numerator * that.denominator - that.numerator * denominator,
+        denominator * that.denominator
+    )
         
-    // naive implementation
-    operator fun plus(that: Rational): Rational {
-        val thisNumerator = this.numerator * that.denominator
-        val thatNumerator = that.numerator * this.denominator
-        return Rational(thisNumerator + thatNumerator, this.denominator * that.denominator)
+    operator fun plus(that: Rational): Rational = createRational(
+       numerator * that.denominator + that.numerator * denominator,
+       denominator * that.denominator)
+
+    operator fun div(that: Rational): Rational = createRational(
+       numerator * that.denominator,
+        denominator * that.numerator)
+
+// Range works automatically in kotlin when comparable is implemented (overrides Comparable interface method)
+    override fun compareTo(other: Rational): Int {
+        return (numerator * other.denominator - other.numerator * denominator).signum()
     }
-
-    operator fun div(that: Rational): Rational {
-        val numerator = this.numerator * that.denominator
-        val denominator = this.denominator * that.numerator
-        return Rational(numerator, denominator)
-    }
-
-    operator fun compareTo(other: Rational): Int {
-        if (this == other) return 0
-        val thisNum = this.numerator * other.denominator
-        val otherNum = other.numerator * this.denominator
-
-        return if (thisNum > otherNum) 1 else -1
-    }
-
-    operator fun rangeTo(toNum: Rational) : RationalNumberRange = RationalNumberRange(this, toNum)
+// Range works automatically in kotlin when comparable is implemented
+//    operator fun rangeTo(toNum: Rational) : RationalNumberRange = RationalNumberRange(this, toNum)
 
     override fun toString(): String {
         if (denominator == BigInteger.ONE)
@@ -72,30 +61,33 @@ data class Rational(private var numerator: BigInteger, private var denominator: 
     }
 }
 
-data class RationalNumberRange(val fromNum: Rational, val toNum: Rational) {
-    operator fun contains(rational: Rational): Boolean
-    = rational >= fromNum && rational <= toNum
-}
+// Range works automatically in kotlin when comparable is implemented - no special class is required
+//data class RationalNumberRange(val fromNum: Rational, val toNum: Rational) {
+//    operator fun contains(rational: Rational): Boolean
+//    = rational >= fromNum && rational <= toNum
+//}
 
 fun String.toRational(): Rational {
-    val splittedStr = this.split("""/""")
-    if (splittedStr.size != 2 && splittedStr.size != 1) throw IllegalArgumentException("Not in the rational number format: " + this)
-
-    val numerator = splittedStr[0].toBigInteger()
-    val denominator = if (splittedStr.size == 1) BigInteger.ONE else splittedStr[1].toBigInteger()
-
-    return Rational(numerator, denominator)
+    fun String.toBigIntegerOrFail() =
+        toBigIntegerOrNull() ?: throw IllegalArgumentException(
+        "Expecting rational in the form of 'numerator/denominator' or 'numerator', was: '${this@toRational}'"
+    )
+    if (!contains("/")) {
+        return Rational.Companion.createRational(toBigIntegerOrFail(), ONE)
+    }
+    val (numeratorText, denominatorText) = this.split("""/""")
+    return Rational.createRational(numeratorText.toBigIntegerOrFail(), denominatorText.toBigIntegerOrFail())
 }
 
-infix fun BigInteger.divBy(denominator: BigInteger): Rational = Rational(this, denominator)
-infix fun BigInteger.divBy(denominator: Long): Rational = Rational(this, denominator.toBigInteger())
-infix fun BigInteger.divBy(denominator: Int): Rational = Rational(this, denominator.toBigInteger())
+infix fun BigInteger.divBy(denominator: BigInteger): Rational = Rational.createRational(this, denominator)
+infix fun BigInteger.divBy(denominator: Long): Rational = Rational.createRational(this, denominator.toBigInteger())
+infix fun BigInteger.divBy(denominator: Int): Rational = Rational.createRational(this, denominator.toBigInteger())
 
-infix fun Long.divBy(denominator: Long): Rational = Rational(this, denominator)
-infix fun Long.divBy(denominator: BigInteger): Rational = Rational(this.toBigInteger(), denominator)
-infix fun Long.divBy(denominator: Int): Rational = Rational(this.toBigInteger(), denominator.toBigInteger())
+infix fun Long.divBy(denominator: Long): Rational = Rational.createRational(this, denominator)
+infix fun Long.divBy(denominator: BigInteger): Rational = Rational.createRational(this.toBigInteger(), denominator)
+infix fun Long.divBy(denominator: Int): Rational = Rational.createRational(this.toBigInteger(), denominator.toBigInteger())
 
-infix fun Int.divBy(denominator: Int): Rational = Rational(this, denominator)
-infix fun Int.divBy(denominator: BigInteger): Rational = Rational(this.toBigInteger(), denominator)
-infix fun Int.divBy(denominator: Long): Rational = Rational(this.toBigInteger(), denominator.toBigInteger())
+infix fun Int.divBy(denominator: Int): Rational = Rational.createRational(this, denominator)
+infix fun Int.divBy(denominator: BigInteger): Rational = Rational.createRational(this.toBigInteger(), denominator)
+infix fun Int.divBy(denominator: Long): Rational = Rational.createRational(this.toBigInteger(), denominator.toBigInteger())
 
